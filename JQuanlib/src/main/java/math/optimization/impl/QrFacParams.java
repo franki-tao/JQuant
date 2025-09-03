6927c55b7f53a2675bcf537b1620e430bde41f4b
tao <2662110275@qq.com>
2025-09-03 20:39:03 +0800

    完成QuantLib::MINPACK编写

--- JQuanlib/src/main/java/math/optimization/impl/QrFacParams.java
+++ JQuanlib/src/main/java/math/optimization/impl/QrFacParams.java
@@ -32,4 +32,18 @@ public class QrFacParams {
     public double[] acnorm;
 
     public double[] wa;
+
+    public QrFacParams(int m, int n, double[] a, int x1, int pivot, int[] ipvt, int x2,
+                       double[] rdiag, double[] acnorm, double[] wa) {
+        this.m = m;
+        this.n = n;
+        this.a = a;
+        this.x1 = x1;
+        this.pivot = pivot;
+        this.ipvt = ipvt;
+        this.x2 = x2;
+        this.rdiag = rdiag;
+        this.acnorm = acnorm;
+        this.wa = wa;
+    }
 }
