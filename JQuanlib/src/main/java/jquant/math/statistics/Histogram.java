ec248be581097c145d3cb4d3c20054ea3818bdc2
furanki-tao <2662110275@qq.com>
2025-12-08 16:15:39 +0800

    完成histogram编写;

---
+++ JQuanlib/src/main/java/jquant/math/statistics/Histogram.java
@@ -0,0 +1,164 @@
+package jquant.math.statistics;
+
+import jquant.math.CommonUtil;
+
+import java.util.Collections;
+import java.util.List;
+
+import static jquant.math.CommonUtil.QL_FAIL;
+import static jquant.math.CommonUtil.QL_REQUIRE;
+import static jquant.math.statistics.impl.StatiticsUtil.quantile;
+import static jquant.math.statistics.impl.StatiticsUtil.unique;
+
+/**
+ * ! Histogram class
+ * ! This class computes the histogram of a given data set.  The
+ * caller can specify the number of bins, the breaks, or the
+ * algorithm for determining these quantities in computing the
+ * histogram.
+ */
+public class Histogram {
+    public enum Algorithm {None, Sturges, FD, Scott}
+
+    private List<Double> data_;
+    private int bins_;
+    private Algorithm algorithm_;
+    private List<Double> breaks_;
+    private List<Integer> counts_;
+    private List<Double> frequency_;
+
+    //! \name constructors
+    //@{
+    public Histogram() {
+        algorithm_ = Algorithm.Scott;
+    }
+
+    public Histogram(List<Double> data, int breaks) {
+        data_ = data;
+        bins_ = breaks + 1;
+        calculate();
+    }
+
+    public Histogram(List<Double> data, Algorithm algorithm) {
+        data_ = data;
+        bins_ = -1;
+        algorithm_ = algorithm;
+        calculate();
+    }
+
+    public Histogram(List<Double> data, List<Double> breaks) {
+        data_ = data;
+        breaks_ = breaks;
+        bins_ = breaks.size() + 1;
+        calculate();
+    }
+
+    //@}
+
+    //! \name inspectors
+    //@{
+    public int bins() {
+        return bins_;
+    }
+
+    public final List<Double> breaks() {
+        return breaks_;
+    }
+
+    public Algorithm algorithm() {
+        return algorithm_;
+    }
+
+    public boolean empty() {
+        return bins_ == 0;
+    }
+    //@}
+
+    //! \name results
+    //@{
+    public int counts(int i) {
+        return counts_.get(i);
+    }
+
+    public double frequency(int i) {
+        return frequency_.get(i);
+    }
+    //@}
+
+    private void calculate() {
+        QL_REQUIRE(!data_.isEmpty(), "no data given");
+
+        double min = Collections.min(data_);  //*std::min_element(data_.begin(), data_.end());
+        double max = Collections.max(data_);  //*std::max_element(data_.begin(), data_.end());
+
+        // calculate number of bins if necessary
+        if (bins_ == -1) {
+            switch (algorithm_) {
+                case Sturges: {
+                    bins_ = (int) Math.ceil(Math.log(data_.size()) / Math.log(2.0) + 1);
+                    break;
+                }
+                case FD: {
+                    double r1 = quantile(data_, 0.25);
+                    double r2 = quantile(data_, 0.75);
+                    double h = 2.0 * (r2 - r1) * Math.pow((data_.size()), -1.0 / 3.0);
+                    bins_ = (int) (Math.ceil((max - min) / h));
+                    break;
+                }
+                case Scott: {
+                    IncrementalStatistics summary = new IncrementalStatistics();
+                    summary.addSequence(data_, 0, data_.size());
+                    double variance = summary.variance();
+                    double h = 3.5 * Math.sqrt(variance)
+                            * Math.pow((data_.size()), -1.0 / 3.0);
+                    bins_ = (int) (Math.ceil((max - min) / h));
+                    break;
+                }
+                case None:
+                    QL_FAIL("a bin-partition algorithm is required");
+                default:
+                    QL_FAIL("unknown bin-partition algorithm");
+            }
+            ;
+            bins_ = Math.max(bins_, 1);
+        }
+
+        if (breaks_.isEmpty()) {
+            // set breaks if not provided
+            breaks_ = CommonUtil.ArrayInit(bins_ - 1);
+
+            // ensure breaks_ evenly span over the range of data_
+            // TODO: borrow the idea of pretty in R.
+            double h = (max - min) / bins_;
+            for (int i = 0; i < breaks_.size(); ++i) {
+                breaks_.set(i, min + (i + 1) * h);
+            }
+        } else {
+            // or ensure they're sorted if given
+            Collections.sort(breaks_);
+            unique(breaks_);
+        }
+
+        // finally, calculate counts and frequencies
+        counts_ = CommonUtil.ArrayInit(bins_, 0);
+
+        for (double p : data_) {
+            boolean processed = false;
+            for (int i = 0; i < breaks_.size(); ++i) {
+                if (p < breaks_.get(i)) {
+                    counts_.set(i, counts_.get(i) + 1);
+                    processed = true;
+                    break;
+                }
+            }
+            if (!processed)
+                counts_.set(bins_ - 1, counts_.get(bins_ - 1) + 1);
+        }
+
+        frequency_ = CommonUtil.ArrayInit(bins_);
+
+        int totalCounts = data_.size();
+        for (int i = 0; i < bins_; ++i)
+            frequency_.set(i, (double) (counts_.get(i)) / totalCounts);
+    }
+}
--- JQuanlib/src/main/java/jquant/math/statistics/impl/StatiticsUtil.java
+++ JQuanlib/src/main/java/jquant/math/statistics/impl/StatiticsUtil.java
@@ -1,4 +1,136 @@
 package jquant.math.statistics.impl;
 
+import jquant.math.CommonUtil;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Comparator;
+import java.util.ListIterator;
+
+import static jquant.math.CommonUtil.QL_REQUIRE;
+import static jquant.math.MathUtils.close_enough;
+
 public class StatiticsUtil {
+    /**
+     * The discontinuous quantiles use the method (type 8) as
+     * recommended by Hyndman and Fan (1996). The resulting
+     * quantile estimates are approximately median-unbiased
+     * regardless of the distribution of 'samples'.
+     * <p>
+     * If quantile function is called multiple times for the same
+     * dataset, it is recommended to pre-sort the sample vector.
+     */
+    public static double quantile(List<Double> samples, double prob) {
+        int nsample = samples.size();
+        QL_REQUIRE(prob >= 0.0 && prob <= 1.0,
+                "Probability has to be in [0,1].");
+        QL_REQUIRE(nsample > 0, "The sample size has to be positive.");
+
+        if (nsample == 1)
+            return samples.get(0);
+
+        // two special cases: close to boundaries
+        final double a = 1. / 3, b = 2 * a / (nsample + a);
+        if (prob < b)
+            return Collections.min(samples); //*std::min_element(samples.begin(), samples.end());
+        else if (prob > 1 - b)
+            return Collections.max(samples); //*std::max_element(samples.begin(), samples.end());
+
+        // general situation: middle region and nsample >= 2
+        int index = (int) Math.floor((nsample + a) * prob + a); //static_cast < Size > (std::floor ((nsample + a) * prob + a));
+        List<Double> sorted = CommonUtil.ArrayInit(index + 1);
+        partialSortCopy(samples, sorted);
+        // use "index & index+1"th elements to interpolate the quantile
+        double weight = nsample * prob + a - index;
+        return (1 - weight) * sorted.get(index - 1) + weight * sorted.get(index);
+    }
+
+    /**
+     * 从源列表 (samples) 中找出最小的 K 个元素，并排序后复制到目标列表 (sorted) 中。
+     * K 由目标列表的当前大小决定。
+     *
+     * @param samples 源 List<Double>，从中选择元素。
+     * @param sorted  目标 List<Double>，必须预先设置好其大小K，用来存放结果。
+     *                注意：此方法会清空 sorted 并重新填充。
+     */
+    public static void partialSortCopy(List<Double> samples, List<Double> sorted) {
+        if (sorted == null) {
+            return;
+        }
+
+        // K 是目标列表的大小，即我们想要找出的最小元素的数量。
+        int k = sorted.size();
+        if (k == 0) {
+            return;
+        }
+
+        // 1. 使用最大堆 (Max-Heap) 来存储 K 个最小元素。
+        // Collections.reverseOrder() 将 PriorityQueue 变为最大堆。
+        PriorityQueue<Double> maxHeap = new PriorityQueue<>(k, Collections.reverseOrder());
+
+        // 2. 遍历源列表 samples
+        for (double sample : samples) {
+            if (maxHeap.size() < k) {
+                // 如果堆还没有满，直接添加元素。
+                maxHeap.offer(sample);
+            } else if (sample < maxHeap.peek()) {
+                // 如果当前元素比堆顶（目前 K 个最小元素中的最大值）还小，
+                // 那么这个元素应该被保留。
+                maxHeap.poll(); // 移除堆中最大的元素
+                maxHeap.offer(sample); // 添加新的更小的元素
+            }
+        }
+
+        // 3. 将堆中的元素按排序后的顺序复制到目标列表 sorted。
+
+        // 优先队列 (PriorityQueue) 默认是最小堆，虽然我们这里用了最大堆的比较器，
+        // 但为了获得排序后的结果，我们可以利用它的特性：
+
+        // 首先清空目标列表
+        sorted.clear();
+
+        // 临时列表用于存储堆中元素，然后排序
+        List<Double> result = new java.util.ArrayList<>(maxHeap);
+
+        // 因为堆中的元素是无序的，我们需要对这 K 个元素进行最后的排序（升序）。
+        // 此时 List 的大小最多为 K，排序成本很低 O(K log K)。
+        Collections.sort(result);
+
+        // 将排序后的结果复制到目标列表中。
+        sorted.addAll(result);
+
+        // **注意**：如果 samples 的大小小于 k，则 sorted 中只有 maxHeap.size() 个元素。
+    }
+
+    /**
+     * 模拟 std::unique 的功能：消除 List 中相邻的、满足谓词关系的重复元素。
+     *
+     * @param breaks 需要处理的 List<Double> (会被修改)
+     */
+    public static void unique(List<Double> breaks) {
+        if (breaks == null || breaks.size() < 2) {
+            return;
+        }
+
+        // 使用 ListIterator 进行安全的删除操作
+        ListIterator<Double> current = breaks.listIterator();
+
+        // previous 是我们保留的最后一个元素。初始化为第一个元素。
+        Double previous = current.next();
+
+        // 从第二个元素开始遍历
+        while (current.hasNext()) {
+            Double currentElement = current.next();
+
+            // 检查当前元素和前一个保留的元素是否“足够接近”
+            if (close_enough(previous, currentElement)) {
+                // 如果相等（足够接近），则删除当前元素
+                current.remove();
+            } else {
+                // 如果不相等，则当前元素成为下一个要比较的“前一个保留元素”
+                previous = currentElement;
+            }
+        }
+    }
 }
